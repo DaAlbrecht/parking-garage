f548f829ba6957e7ca69ead9b8f7699862226373
David <dadidi42@gmail.com>
2023-02-19 23:46:46 +0100

    ADD: heading for each level

--- src/routes/admin/details/[slug]/+page.svelte
+++ src/routes/admin/details/[slug]/+page.svelte
@@ -4,6 +4,7 @@
 </script>
 
 {#each data.levels as level }
+    <h1>level: {level.levelNumber}</h1>
     {#each Array(level.parking_spaces) as _, index(index)}
         {#if data.occupiedParkingSpaces.includes(index)}
             <div class="bg-red-600">{index}</div>
