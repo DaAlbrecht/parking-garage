b1312b83cfccd131bcb72280a0261afb4f5782d8
DaAlbrecht <dadidi42@gmail.com>
2023-03-19 10:46:54 +0100

    ADD: add long term customer on parkingspace util

--- src/lib/util/parkingSpaceUtil.ts
+++ src/lib/util/parkingSpaceUtil.ts
@@ -5,6 +5,7 @@ export interface LevelParkingSpace {
   parkingSpot: number;
   occupied: boolean;
   level_id: number;
+  permanentTenant: boolean;
 }
 async function getOccupiedParkingSpacesForLevel(level: Level): Promise<Array<number>> {
   const parkingSpaces = await prisma.parkingSpace.findMany({
@@ -19,11 +20,36 @@ export async function getAllParkingSpacesForLevel(level: Level): Promise<LevelPa
   const occupiedParkingSpaces = await getOccupiedParkingSpacesForLevel(level);
   const parkingSpaces = new Array<LevelParkingSpace>();
   for (let i = 0; i < level.parking_spaces; i++) {
-    parkingSpaces.push({
-      parkingSpot: i,
-      occupied: occupiedParkingSpaces.includes(i),
-      level_id: level.id
-    });
+    if (occupiedParkingSpaces.includes(i)) {
+      const parkingSpace = await prisma.parkingSpace.findFirst({
+        where: {
+          level_id: level.id,
+          parkingSpot: i
+        }
+      });
+      if (!parkingSpace) continue;
+
+      const customer = await prisma.customer.findFirst({
+        where: {
+          id: parkingSpace.customer_id
+        }
+      });
+      if (!customer) continue;
+
+      parkingSpaces.push({
+        parkingSpot: i,
+        occupied: true,
+        level_id: level.id,
+        permanentTenant: customer.is_long_term_customer
+      });
+    } else {
+      parkingSpaces.push({
+        parkingSpot: i,
+        occupied: false,
+        level_id: level.id,
+        permanentTenant: false
+      });
+    }
   }
   return parkingSpaces;
 }
